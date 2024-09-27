0744cfe9db457d1dfdb13419c34411bcc306b57c
ChalitaK24 <chalurga@gmail.com>
2024-09-26 18:47:02 -0700

    push m

---
+++ pushmonty.c
@@ -0,0 +1,37 @@
+#include "monty.h"
+
+/**
+ * push_m - adds an integer to the stack
+ *
+ *
+ * Return: new code
+ */
+
+void push_m(stack_t **stack, unsigned int line_num, char *arg)
+{
+	if (arg == NULL || !is_integer(arg))
+	{
+		fprintf(stderr, "L%d: usage: push integer\n", line_num);
+		exit(EXIT_FAILURE);
+	}
+
+	int value = atoi(arg);
+	stack_t *new_node = malloc(sizeof(stack_t));
+
+	if (new_node == NULL)
+	{
+		fprintf(stderr, "Error: malloc failed\n");
+		exit(EXIT_FAILURE);
+	}
+
+	new_node->n = value;
+	new_node->prev = NULL;
+	new_node->next = *stack;
+
+	if (*stack != NULL)
+	{
+		(*stack)->prev = new_node;
+	}
+
+	*stack = new_node;
+}
